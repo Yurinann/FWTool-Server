ce3d22bdb99b8bdd56d98a00bec24c680024b89e
Yurinann <1713574450@qq.com>
2022-03-25 16:22:07 +0800

    [ci skip] Update Test

--- src/test/java/ColorParseTest.java
+++ src/test/java/ColorParseTest.java
@@ -10,8 +10,9 @@ public class ColorParseTest {
 
     @Test
     public void onTest() {
-        String testString = "&fI send my thoughts from time immemorial to eternity to Ghost_You. I had flipped, completely.";
-        String testHexString = "&(#FFFFFF)You can do you want cause I love you, baby. --- << Indulge >>";
+        String testString = "&fI send the love of the whole universe and my thoughts" +
+                " from the beginning of time to the end of time to my beloved, Ghost You.";
+        String testHexString = "&(#FFFFFF)将全宇宙的爱与自太古至永劫的思念寄给我的爱人, 魂优。";
         System.out.println(ColorParser.parse(testString));
         System.out.println(ColorParser.parse(testHexString));
     }
